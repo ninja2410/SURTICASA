308eed007067f21dca3f408bb805bead600ff53e
José Carlos Santiago <santiago.jose@outlook.com>
2018-06-04 05:32:58 -0600

    Agregando reporte Clientes Morosos

---
+++ ProyectoFinal/ProyectoFinal/reporteClientesMorosos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace ProyectoFinal
+{
+    public partial class reporteClientesMorosos : DevExpress.XtraReports.UI.XtraReport
+    {
+        public reporteClientesMorosos()
+        {
+            InitializeComponent();
+        }
+        public void InitData(DateTime fechaActual)
+        {
+            sqlDataSource1.Queries[0].Parameters[0].Value = fechaActual;
+
+        }
+
+
+    }
+}
