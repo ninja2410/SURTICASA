c2a40ee4a155c5a335c2e5be871f4500ad77dd28
José Carlos Santiago <santiago.jose@outlook.com>
2018-06-04 01:16:16 -0600

    reporte Ventas por Empleado y Cliente

---
+++ ProyectoFinal/ProyectoFinal/reporteVentasClieneEmpleado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace ProyectoFinal
+{
+    public partial class reporteVentasClieneEmpleado : DevExpress.XtraReports.UI.XtraReport
+    {
+        public reporteVentasClieneEmpleado()
+        {
+            InitializeComponent();
+        }
+
+        public void InitData(string idCliente)
+        {
+            sqlDataSource1.Queries[0].Parameters[0].Value = idCliente;
+
+        }
+    }
+}
